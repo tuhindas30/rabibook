096c1d83aa739879caf058032582d213d0c9bb21
Tuhin Das <mailme.tuhindas@gmail.com>
2021-09-18 01:31:56 +0530

    fix: click on avatar or username takes to profile page 🔧

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { Image, Row, Col, ListGroup } from "react-bootstrap";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
@@ -16,6 +16,7 @@ const PostCard = ({ post, onLike, onRemoveLike, onDelete, onComment }) => {
   const isCurrentUserPost = post.author.uid === uid ? true : false;
   const isPostLiked = post.likedBy?.includes(uid);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { isLiked, setLiked, likePost, removeLike } = useLike(
     post.postId,
     post?.likedBy
@@ -25,21 +26,29 @@ const PostCard = ({ post, onLike, onRemoveLike, onDelete, onComment }) => {
     setLiked(isPostLiked);
   }, [isPostLiked, setLiked]);
 
-  const handleLike = async () => {
+  const handleAvatarOrNameClick = (e, username) => {
+    e.stopPropagation();
+    navigate(`/${username}`);
+  };
+
+  const handleLike = async (e) => {
+    e.stopPropagation();
     const response = await likePost(post.postId, uid);
     if (response) {
       onLike(post.postId);
     }
   };
 
-  const handleRemoveLike = async () => {
+  const handleRemoveLike = async (e) => {
+    e.stopPropagation();
     const response = await removeLike(post.postId, uid);
     if (response) {
       onRemoveLike(post.postId);
     }
   };
 
-  const handleComment = (postId, uid) => {
+  const handleComment = (e, postId, uid) => {
+    e.stopPropagation();
     const data = {
       displayName,
       username,
@@ -50,52 +59,63 @@ const PostCard = ({ post, onLike, onRemoveLike, onDelete, onComment }) => {
   };
 
   return (
-    <ListGroup.Item>
-      <Link to={`/post/${post.postId}`}>
-        <Row className={styles.listHeader}>
-          <Col xs={2}>
-            <Image
-              src={post.author.avatar}
-              className={styles.listAvatar}
-              roundedCircle
-            />
-          </Col>
-          <Col>
-            <div className={styles.listTitle}>
-              <span className={styles.displayName}>
-                {post.author.displayName}
-              </span>
-              <span className={styles.username}>@{post.author.username}</span>
-              &middot;
-              <span className={styles.postTime}>
-                {dateToRelativeTime(post.createdAt)}
-              </span>
-            </div>
-            <div>{post.content}</div>
-          </Col>
-        </Row>
-      </Link>
+    <ListGroup.Item
+      className={styles.card}
+      onClick={() => navigate(`/post/${post.postId}`)}>
+      <Row className={styles.listHeader}>
+        <Col xs={2}>
+          <Image
+            src={post.author.avatar}
+            className={styles.listAvatar}
+            onClick={(e) => handleAvatarOrNameClick(e, post.author.username)}
+            roundedCircle
+          />
+        </Col>
+        <Col>
+          <div className={styles.listTitle}>
+            <span
+              className={styles.displayName}
+              onClick={(e) => handleAvatarOrNameClick(e, post.author.username)}>
+              {post.author.displayName}
+            </span>
+            <span
+              className={styles.username}
+              onClick={(e) => handleAvatarOrNameClick(e, post.author.username)}>
+              @{post.author.username}
+            </span>
+            &middot;
+            <span className={styles.postTime}>
+              {dateToRelativeTime(post.createdAt)}
+            </span>
+          </div>
+          <div>{post.content}</div>
+        </Col>
+      </Row>
       <Row>
         <Col className={styles.footerItem}>
-          <BiComment
-            onClick={() => handleComment(post.postId, uid)}
-            className={`pointer ${styles.iconMargin}`}
-          />
-          <span>{post.commentCount}</span>
+          <div className={styles.commentBtn}>
+            <BiComment
+              onClick={(e) => handleComment(e, post.postId, uid)}
+              className={`pointer ${styles.commentIcon}`}
+            />
+            <span className={styles.count}>{post.commentCount}</span>
+          </div>
         </Col>
         <Col className={styles.footerItem}>
-          {isLiked ? (
-            <AiFillHeart
-              onClick={handleRemoveLike}
-              className={`pointer ${styles.iconMargin} ${styles.iconColor}`}
-            />
-          ) : (
-            <AiOutlineHeart
-              onClick={handleLike}
-              className={`pointer ${styles.iconMargin}`}
-            />
-          )}
-          <span>{post.likedBy.length}</span>
+          <div className={styles.likeBtn}>
+            {isLiked ? (
+              <AiFillHeart
+                onClick={handleRemoveLike}
+                className={`pointer ${styles.likeIcon} ${styles.iconColor}`}
+              />
+            ) : (
+              <AiOutlineHeart
+                onClick={handleLike}
+                className={`pointer ${styles.likeIcon}`}
+              />
+            )}
+            <span className={styles.count}>{post.likedBy.length}</span>
+          </div>
         </Col>
         {isCurrentUserPost && (
           <Col className={styles.footerItem}>
